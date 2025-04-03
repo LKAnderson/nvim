64e7d9164667e63820a66a323ec2ddccc3e74a2c
Kent Anderson <kent@pretaa.com>
2025-04-03 15:15:15 -0700

    More Telescope

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -40,8 +40,8 @@ vim.g.netrw_list_hide = ".DS_Store"
 
 vim.cmd "nnoremap t :NvimTreeFocus<CR>"
 -- vim.cmd "nnoremap gb :ls<CR>:b "
--- vim.cmd "nnoremap gm :lua vim.diagnostic.open_float()<CR>"
--- vim.cmd "nnoremap gca :lua vim.lsp.buf.code_action()<CR>"
+vim.cmd "nnoremap gm :lua vim.diagnostic.open_float()<CR>"
+vim.cmd "nnoremap gca :lua vim.lsp.buf.code_action()<CR>"
 
 -- Setup autosave
 vim.opt.updatetime = 1000
--- lua/core/plugin_configs/telescope.lua
+++ lua/core/plugin_configs/telescope.lua
@@ -1,6 +1,4 @@
 vim.cmd "nnoremap gtf :Telescope find_files<CR>"
 vim.cmd "nnoremap gtg :Telescope live_grep<CR>"
 vim.cmd "nnoremap gb :Telescope buffers<CR>"
-vim.cmd "nnoremap gca :Telescope lsp_code_actions<CR>"
-vim.cmd "nnoremap gm :Telescope diagnostics<CR>"
 
