c0f73bac543f5537a10b3e091fdbb2da6787b2cd
Kent Anderson <kent@pretaa.com>
2025-05-02 15:16:30 -0700

    add null-ls and prettier formatting

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   {"williamboman/mason-lspconfig.nvim"},
   {"neovim/nvim-lspconfig"},
   {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+  {"nvimtools/none-ls.nvim", dependencies = { "nvim-lua/plenary.nvim", }},
   {"hrsh7th/cmp-nvim-lsp"},
   {"hrsh7th/nvim-cmp"},
   {"L3MON4D3/LuaSnip"},
